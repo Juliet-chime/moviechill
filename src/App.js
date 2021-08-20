b34077ac512c53397eeb7a23a6880703771f37ab
Chime Juliet <annychime@gmail.com>
2021-08-19 21:17:18 -0700

    deleted a variable

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
   const [favourite, setFavourite] = useState([])
 
   const getMovieRequest = async (searchValue) => {
-    const star = "star"
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
     const response = await fetch(url)
     const responseJson = await response.json()
